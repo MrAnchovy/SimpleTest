ff717892817c0aa316ff16911214b8b055ab4edd
MrAnchovy <mr.anchovy@mranchovy.com>
2013-11-16 03:06:07 +0000

    Beginning to implement expect/matcher tests

---
+++ classes/Sins/TestCase/ExpectMatcherTestCase.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Sins\TestCase;
+
+// TODO build the expectations in expectation.php
+
+
+class ExpectMatcherTestCase extends \SimpleTestCase {
+
+    private $value;
+    private $expected;
+    private $not;
+
+    function expect($value)
+    {
+        $this->expected = true;
+        $this->value = $value;
+        return $this;
+    }
+
+    private function expected()
+    {
+        if ($this->expected) {
+            $this->expected = false;
+            return $this->value;
+        } else {
+            throw new \Exception('Expectation not set');
+        }
+    }
+
+    function not()
+    {
+        $this->not = true;
+        return $this;
+    }
+
+    function toBeFalse($message = null)
+    {
+        if ($this->not) {
+            $this->assert(new \FalseExpectation(), $this->expected() === false, "$message (%s)");
+            $this->not = false;
+        } else {
+            $this->assert(new \TrueExpectation(), $this->expected() === false, "$message (%s)");
+        }
+    }
+
+    function toBeNull($message = null)
+    {
+        if ($this->not) {
+            $this->assert(new \FalseExpectation(), $this->expected() === null, "$message (%s)");
+            $this->not = false;
+        } else {
+            $this->assert(new \TrueExpectation(), $this->expected() === null, "$message (%s)");
+        }
+    }
+
+    function toBe($value, $message = null)
+    {
+        if ($this->not) {
+            $this->assert(new \FalseExpectation(), $this->expected() === $value, "$message (%s)");
+            $this->not = false;
+        } else {
+            $this->assert(new \TrueExpectation(), $this->expected() === $value, "$message (%s)");
+        }
+    }
+
+    function toBeTrue($message = null)
+    {
+        if ($this->not) {
+            $this->assert(new \FalseExpectation(), $this->expected() === true, "$message (%s)");
+            $this->not = false;
+        } else {
+            $this->assert(new \TrueExpectation(), $this->expected() === true, "$message (%s)");
+        }
+    }
+
+    function toBeTruthy($message = null)
+    {
+        if ($this->not) {
+            $this->assert(new \FalseExpectation(), $this->expected() == true, "$message (%s)");
+            $this->not = false;
+        } else {
+            $this->assert(new \TrueExpectation(), $this->expected() == true, "$message (%s)");
+        }
+    }
+}
