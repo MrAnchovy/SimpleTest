23e38e0a188aea992fed035bd2a68fe67bf30d3e
MrAnchovy <mr.anchovy@mranchovy.com>
2013-12-03 09:49:49 +0000

    Improve file listing

--- classes/Sins/Controller/Controller_file.php
+++ classes/Sins/Controller/Controller_file.php
@@ -10,16 +10,60 @@
 namespace Sins\Controller;
 
 use Sins\TestFiles;
+use Exception;
 
 class Controller_file extends \Sins\Controller {
 
+
     /**
-     * Implement /run/[id]/autorun.json.
+     * Implement `file/[id]/list.json`.
     **/
-    function executeApiAction_list($id = null) {
+    protected function executeApiAction_list($id = null)
+    {
         $files = new TestFiles;
+
+        $path = realpath($id === '.' ? $this->local->testBaseDir : $this->local->testBaseDir . DIRECTORY_SEPARATOR . $id);
+
+        // path must exist and be under the base directory - defeat directory traversal with ..
+        if (empty($path) || strpos($path, $this->local->testBaseDir) !== 0 ) {
+            $this->response->status = 404;
+            $this->response->body = array(
+                'status' => 'error',
+                'error'  => array('message' => 'Invalid path'),
+            );
+            return;
+        }
+
+        $files->stripBase = $this->local->testBaseDir . DIRECTORY_SEPARATOR;
+
+        $testFiles = $files->scan($path);
+
+        $this->response->body = array(
+            'status' => 'ok',
+            'files'  => $testFiles,
+        );
+    }
+
+    /**
+     * Implement /file/[id]/list.json.
+    **/
+    protected function list_sins_tests()
+    {
+        $files = new TestFiles;
+
         $path = $this->local->baseDir.'test';
-        return $files->recursiveScan($path);
+        $testFiles = $files->scan($path);
+
+        $path = $this->local->baseDir.'simpletest/test';
+        $files->allFiles  = true;
+        $files->recursive = false;
+
+        $testFiles = array_merge($testFiles, $files->scan($path));
+
+        $this->response->body = array(
+            'status' => 'ok',
+            'files'  => $testFiles,
+        );
     }
 
 }
--- classes/Sins/TestFiles.php
+++ classes/Sins/TestFiles.php
@@ -2,23 +2,92 @@
 
 namespace Sins;
 
+use FilesystemIterator;
+
 class TestFiles
 {
 
-    public function recursiveScan($paths) {
-        if (!is_array($paths)) {
-            $paths = array($paths);
-        }
-        foreach ($paths as $path) {
-            $path = realpath($path);
-            $objects = new RecursiveIteratorIterator(
-                new RecursiveDirectoryIterator($path),
-                RecursiveIteratorIterator::SELF_FIRST);
-            foreach($objects as $name => $object){
-                echo "$name\n";
+    /**
+     * Set to true to include files in all directories, otherwise only those matching
+     * $testDirPattern will be scanned and in addition, no subdirectories of a
+     * directory matching $this->ignoreDirPattern will be scanned.
+    **/
+    public $allDirs = false;
+
+    /**
+     * Set to true to include all files in scanned directories, otherwise only those
+     * matching $testFilePattern will be included.
+    **/
+    public $allFiles = false;
+
+    /**
+     * Directories to ignore. The default ignores e.g. `.git`.
+    **/
+    public $ignoreDirPattern = '@^\.@';
+
+    /**
+     * Base directory to strip from filepaths.
+    **/
+    public $stripBase;
+
+    /**
+     * Directories to include. The default includes anything starting with test.
+    **/
+    public $testDirPattern = '@^(test)@i';
+
+    /**
+     * Files to include. The default includes anything starting with test_ or
+     * testsuite_
+    **/
+    public $testFilePattern = '@^(test_|testsuite_)@i';
+
+    /**
+     * Scan recursively.
+    **/
+    public $recursive = true;
+
+    /**
+     * Scan recursively for test/testsuite files.
+     *
+     * @param  string  Path to search.
+     * @param  bool    If false, ignore files until a directory matching
+     *                 $this->testDirPattern is found.
+    **/
+    public function scan($path, $files = true) {
+
+        $found = array();
+        if (is_array($path)) {
+            foreach($path as $onePath) {
+                $found = array_merge($found, $this->recursiveScan($onePath));
             }
         }
-
+        $entries = new FilesystemIterator(realpath($path), FilesystemIterator::SKIP_DOTS);
+        foreach ($entries as $entryPath => $entry) {
+            $entryFilename = $entry->getFilename();
+            if ($entry->isDir()) {
+                // check this directory is not to be ignored
+                if ($this->recursive 
+                    && ($this->allDirs || !preg_match($this->ignoreDirPattern, $entryFilename)))
+                {
+                    // set the flag if files in this directory are to be included
+                    $subFiles = $files || preg_match($this->testDirPattern, $entryFilename);
+                    // recurse, merging the results
+                    $found = array_merge($found, $this->scan($entryPath, $subFiles));
+                }
+            } else {
+                if ($files) {
+                    if ($this->allFiles
+                        || preg_match($this->testFilePattern, $entryFilename, $matches))
+                    {
+                        if ($this->stripBase && substr($entryPath, 0, strlen($this->stripBase)) === $this->stripBase) {
+                            $found[] = substr($entryPath, strlen($this->stripBase));
+                        } else {
+                            $found[] = $entryPath;
+                        }
+                    }
+                }
+            }
+        }
+        return $found;
     }
-
 }
